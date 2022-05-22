1f147b0f1944a15f0cd8e1886c1564ba9fd796fc
abdulazizrufatov <arufatov7@gmail.com>
2022-05-22 19:20:17 +0500

    initial commmit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+alert ("Xisoblagich")
+const money = Number(prompt("Summangizni kiriting"));
+const counter = 9642191;
+
+if (money >= counter){
+    console.log("Oq yo’l, Alisher!");
+}else{
+    console.log(`pulingiz ${money-counter} so'm yetmas ekan
+    Alisher, ozgina sabr qilish kerak bo’lar ekan.
+    Yaxshilab ishlang.`);
+}
